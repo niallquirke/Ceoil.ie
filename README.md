65ab1ab9144fa792f48d937c39992c65e82609a7
Niall Quirke <quirken@amazon.com>
2020-04-14 21:36:21 +0100

    Initial Nuxt.js commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Ceoil.ie
-A platform for discovering Irish music
+
+> A platform to discover Irish music
+
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/index.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="container">
+    <div>
+      <div class="form">
+        <h1 class="form_option">Discover</h1>
+        <select name="Genre" id="genre">
+          <option v-for="genre in genres">{{ genre }}</option>
+        </select>
+        <h1 class="form_option">From</h1>
+        <select name="County" id="county">
+          <option v-for="county in counties">{{ county }}</option>
+        </select>
+      </div>
+      <div class="playlist">
+        <iframe
+          src="https://open.spotify.com/embed/playlist/2jRcgzF55Zfcj7UDZMi7cm"
+          width="300"
+          height="380"
+          frameborder="0"
+          allowtransparency="true"
+          allow="encrypted-media"
+        ></iframe>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  data() {
+    return {
+      genres: [
+        'All Music',
+        'Electronic/Dance Music',
+        'Folk/Acoustic Music',
+        'Pop Music',
+        'Rap/Hiphop Music',
+        'Rock/Metal Music',
+        'Trad Music'
+      ],
+      counties: [
+        'Antrim',
+        'Armagh',
+        'Carlow',
+        'Cavan',
+        'Clare',
+        'Cork',
+        'Derry',
+        'Donegal',
+        'Down',
+        'Dublin',
+        'Fermanagh',
+        'Galway',
+        'Kerry',
+        'Kildare',
+        'Kilkenny',
+        'Laois',
+        'Leitrim',
+        'Limerick',
+        'Longford',
+        'Louth',
+        'Mayo',
+        'Meath',
+        'Monaghan',
+        'Offaly',
+        'Roscommon',
+        'Sligo',
+        'Tipperary',
+        'Tyrone',
+        'Waterford',
+        'Westmeath',
+        'Wexford',
+        'Wicklow'
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+html {
+  background-color: #39b982;
+}
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.form {
+  margin: 50px;
+}
+
+.form_option {
+  margin: 10px;
+}
+
+.playlist {
+  margin: 50px;
+}
+
+select {
+  width: 100%;
+  height: 52px;
+  padding: 0 24px 0 10px;
+  vertical-align: middle;
+  background: #fff
+    url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 4 5'%3E%3Cpath fill='%23343a40' d='M2 0L0 2h4zm0 5L0 3h4z'/%3E%3C/svg%3E")
+    no-repeat right 12px center;
+  background-size: 8px 10px;
+  border: solid 1px rgba(0, 0, 0, 0.4);
+  border-radius: 10px;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  font-size: 1.2em;
+}
+
+select:focus {
+  border-color: #39b982;
+  outline: 0;
+}
+
+select:focus::ms-value {
+  color: #000;
+  background: #fff;
+}
+
+select::ms-expand {
+  opacity: 0;
+}
+</style>
